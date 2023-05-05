f120c24b51ea16923220c14c82b8d0596c3ff4f4
the-name-romel <racopra@sorsu.edu.ph>
2023-05-05 09:34:26 +0800

    initialize and installing packages

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_NqHebAmy0-Y9yZPGcidqbOtH1gfbtOg",
+  authDomain: "youchat-422a4.firebaseapp.com",
+  projectId: "youchat-422a4",
+  storageBucket: "youchat-422a4.appspot.com",
+  messagingSenderId: "329682312052",
+  appId: "1:329682312052:web:f15898debeca0c831b2a21",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
